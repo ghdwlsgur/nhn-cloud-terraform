56adbb87d9b5237726b2e39d3a96e9db6848df80
HongJinHyeok <redmax45@naver.com>
2023-05-05 00:44:06 +0900

    combine each block

--- attack-node-group/output.tf
+++ attack-node-group/output.tf
@@ -1,12 +1,4 @@
 // 생성된 Floating IP 주소
-output "floating_ip-1" {
-  value = openstack_compute_floatingip_v2.fip[0].address
-}
-
-output "floating_ip-2" {
-  value = openstack_compute_floatingip_v2.fip[1].address
-}
-
-output "floating_ip-3" {
-  value = openstack_compute_floatingip_v2.fip[2].address
+output "floating_ip" {
+  value = [openstack_compute_floatingip_v2.fip[*].address]
 }
